d025ded2aefc1e2b0bafbc5c745bef50bb7c311d
rebornwhu <rebornwhu@gmail.com>
2016-01-09 14:00:57 -0800

    Build another segmented controller

--- QuartzFun/ViewController.swift
+++ QuartzFun/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorControl: UISegmentedControl!
+    
+    @IBAction func changeColor(sender: UISegmentedControl) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
